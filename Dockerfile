f417ca1a8acca2cd43a3bbed7d94e6461c2e25b1
Edan Idzerda <19670937+edanidzerda@users.noreply.github.com>
2024-10-02 13:32:09 -0400

    Cleaned up code and metric creation

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM python:3.7-slim-buster
+
+
+# Set the working directory in the container
+WORKDIR /app
+RUN apt-get update && apt-get install -y \
+    python3-pip \
+    python3-dev \
+    build-essential \
+    libssl-dev \
+    libffi-dev \
+    python3-setuptools \
+    python3-venv \
+    && apt-get clean
+
+# Copy the dependencies file to the working directory
+COPY requirements.txt .
+
+# Install any dependencies
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
+# Copy the content of the local src directory to the working directory 
+COPY . .
+
+# Run ups-monitor2.py when the container launches
+CMD ["python", "ups-monitor2.py"]
--- ups-monitor2.py
+++ ups-monitor2.py
@@ -1,14 +1,15 @@
 import os
 import time
-from newrelic_telemetry_sdk import GaugeMetric, CountMetric, SummaryMetric, MetricClient, LogClient
 import subprocess
+from newrelic_telemetry_sdk import GaugeMetric, CountMetric, SummaryMetric, MetricClient, LogClient
+
 
 # Don't forget to set the NEW_RELIC_LICENSE_KEY environment variable
-
+# before running this script
 def get_ups_status():
     try:
         # Run the upsc command to get UPS status
-        result = subprocess.run(['upsc', 'myups'], capture_output=True, text=True)
+        result = subprocess.run(['upsc', 'myups'], capture_output=True, text=True, check=True)
         if result.returncode != 0:
             print('Error getting UPS status:', result.stderr)
             return None
@@ -24,54 +25,42 @@ def get_ups_status():
         print('Exception while getting UPS status:', str(e))
         return None
 
+def create_metric(status, name, units):
+    """create a metric from the UPS status dictionary"""
+    if status:
+        value = float(status.get(name, 0.0))
+        metric = GaugeMetric(name, value, {"units": units})
+        return metric
+    else:
+        return None
+
 
 def send_metrics():
+    """send UPS metrics to New Relic"""
     status = get_ups_status()
     # pretty print status with formatting
     if status:
-        
-        health = float(status.get('battery.charge', 0.0))
-        load = float(status.get('ups.load', 0.0))
-        battery_voltage = float(status.get('battery.voltage', 0.0))
-        input_voltage = float(status.get('input.voltage', 0.0))
-        battery_runtime = float(status.get('battery.runtime', 0.0))
-        power_status = status.get('ups.status')
+        batch = [
+            create_metric(status, "battery.charge", "%"),
+            create_metric(status, "ups.load", "%"),
+            create_metric(status, "battery.voltage", "V"),
+            create_metric(status, "input.voltage", "V"),
+            create_metric(status, "battery.runtime", "seconds"),
+        ]
+        batch.append(GaugeMetric("ups.status", 1 if status.get('ups.status') == "OL" else 0, {"units": "boolean"}))
 
-        status = get_ups_status()
-        if status:
-            health = float(status.get('battery.charge', 0.0))
-            load = float(status.get('ups.load', 0.0))
-            battery_voltage = float(status.get('battery.voltage', 0.0))
-            input_voltage = float(status.get('input.voltage', 0.0))
-            battery_runtime = float(status.get('battery.runtime', 0.0))
-            power_status = status.get('ups.status')
+        metric_client = MetricClient(os.environ["NEW_RELIC_LICENSE_KEY"])
 
-            metrics = {
-                "battery.charge": {"value": health, "units": "%"},
-                "ups.load": {"value": load, "units": "%"},
-                "battery.voltage": {"value": battery_voltage, "units": "V"},
-                "input.voltage": {"value": input_voltage, "units": "V"},
-                "battery.runtime": {"value": battery_runtime, "units": "seconds"},
-                "ups.status": {"value": power_status, "units": ""}
-            }
+        for metric in batch:
+            print ("Sending metric: ", metric)
+            # batch.append(metric)
 
-            metric_client = MetricClient(os.environ["NEW_RELIC_LICENSE_KEY"])
-            batch = []
-
-            for name, data in metrics.items():
-                print ("Sending metric: ", name)
-                metric = GaugeMetric(name, data["value"], {"units": data["units"]})
-                batch.append(metric)
-
-            response = metric_client.send_batch(batch)
-            response.raise_for_status()
-            print("Sent metrics successfully!")
-            print('Health:', health)
-            print('Status:', power_status)
-        else:
-            print('Battery not found')
+        response = metric_client.send_batch(batch)
+        response.raise_for_status()
+        print("Sent metrics successfully!")
     else:
         print('Battery not found')
+        # TODO write event to new relic
 
 while True:
     send_metrics()
