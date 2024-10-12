4aceb0421389232abb2301821d9c4e149766afd4
Edan Idzerda <19670937+edanidzerda@users.noreply.github.com>
2024-10-12 16:58:57 -0400

    Added Python Flask to add a mini web server, and created a background thread to fetch UPS metrics every 5 minutes (hard coded).  Added Jinja to make some variables available on the index immediately.
    Added writing metrics to SQLLite and saving the metrics as JSON, to make it flexible to save all metrics and query by any metric later.
    
    Added REST endpoints to fetch the metric keys, and to fetch metric history by key and last 'N minute' date range.  Timestamps are converted to localtime by the query.

--- ups_monitor.py
+++ ups_monitor.py
@@ -5,15 +5,26 @@ import os
 import time
 from PyNUTClient import PyNUT
 from newrelic_telemetry_sdk import GaugeMetric, MetricClient, LogClient
+import threading
+from flask import Flask, jsonify, render_template, request
+from datetime import datetime
+import json
+import sqlite3
+
+database_name = 'example.db'
 
 # Don't forget to set the NEW_RELIC_LICENSE_KEY environment variable
 # before running this script
-
-battery_metrics = ['battery.charge', 
+global_status = {}
+desired_metrics = {}
+battery_metrics = ['battery.charge',
                    'ups.load', 
                    'battery.voltage', 
                    'input.voltage', 
                    'battery.runtime']
+
+def get_iso8601_timestamp():
+    return datetime.now().isoformat() + 'Z'
 
 def get_ups_status(host='localhost', login='', password=''):
     """get the status of the UPS from the NUT server"""
@@ -26,22 +37,25 @@ def get_ups_status(host='localhost', login='', password=''):
         ups = PyNUT.PyNUTClient(host)
     else:
         ups = PyNUT.PyNUTClient(host=host, login=login, password=password)
-
+    ups_vars = {}
+    
     try:
         for name in ups.GetUPSNames():
             print ("Found UPS: ", name)
-            print (ups.CheckUPSAvailable(ups=name))
             available = ups.CheckUPSAvailable(ups=name)
             if available:
                 result = ups.GetUPSVars(ups=name)
                 # convert binary values in response dict to plain strings
                 ups_vars = {key.decode('utf-8'): value.decode('utf-8') for key, value in result.items()}
-                return ups_vars
     except Exception as e:
         print(f"Failed to connect to NUT server: {e}")
-        return None
+        ups_vars["error"] = str(e)
 
-    return None
+    ups_vars["ups.host"] = host
+    ups_vars["ups.login"] = login
+    ups_vars["timestamp"] = get_iso8601_timestamp()
+
+    return ups_vars
 
 
 def create_metric(ups_status, name, units):
@@ -58,11 +72,11 @@ def get_metrics(status):
     metrics = []
     for metric in battery_metrics:
         if status.get(metric):
+            desired_metrics[metric] = status.get(metric)
             metrics.append(create_metric(status, metric, "%"))
     metrics.append(GaugeMetric("ups.status", 1 if status.get('ups.status') == "OL" else 0, {"units": "boolean"}))
 
     return metrics
-
 
 def send_metrics(status):
     """send UPS metrics to New Relic"""
@@ -90,6 +104,30 @@ def send_metrics(status):
         print('Battery not found')
         LogClient(os.environ["NEW_RELIC_LICENSE_KEY"]).send("error", "Battery not found")
 
+# Run this block in a background thread
+
+def background_ups_monitor():
+    """This function will run in a background thread to monitor the UPS status"""
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    while True:
+        print("Checking UPS stats on thread: ",threading.current_thread())
+        status = get_ups_status(ups_host, ups_login, ups_password)
+        if status:
+            write_to_db(status)
+            global_status.update(status)
+            send_metrics(status)
+        time.sleep(300)
+
+
+def write_to_db(stats):
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    c.execute(''' INSERT INTO metrics (data) VALUES (?)''',(json.dumps(stats),))
+    print("Writing to DB: ", desired_metrics)
+    conn.commit()
+    conn.close()
+
 
 # ups = os.environ.get("UPS_NAME", "myups")
 ups_host = os.environ.get("UPS_HOST", "localhost")
@@ -98,13 +136,112 @@ ups_password = os.environ.get("UPS_PASSWORD", "")
 if os.environ.get("UPS_BATTERY_METRICS"):
     battery_metrics = os.environ["UPS_BATTERY_METRICS"].split(",")
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', global_status=desired_metrics)
+
+@app.route('/metrics')
+def metrics_route():
+    """return the current UPS status as JSON"""
+    return jsonify(global_status)
+
+# @app.route('/metrics/history')
+# def metrics_history():
+#     key = request.args.get('key', 'battery.charge')  # Default to 'battery.charge' if no key is provided
+#     conn = sqlite3.connect(database=database_name)
+#     c = conn.cursor()
+    
+#     # Use parameterized query to prevent SQL injection
+#     query = f'SELECT timestamp, json_extract(data, ?) AS value FROM metrics ORDER BY timestamp DESC'
+#     c.execute(query, (f'$."{key}"',))
+#     rows = c.fetchall()
+#     conn.close()
+    
+#     history = [{'timestamp': row[0], 'value': row[1]} for row in rows]
+    
+#     return jsonify(history)
+
+@app.route('/metrics/history')
+def metrics_history():
+    key = request.args.get('key', 'battery.charge')  # Default to 'battery.charge' if no key is provided
+    dateRange = request.args.get('range', '60')
+    print("Date Range: ", dateRange)
+    conn = sqlite3.connect(database_name)
+    c = conn.cursor()
+    
+    # Use parameterized query to prevent SQL injection
+    query = f'''
+    SELECT strftime('%H:%M', datetime(timestamp, 'localtime')) as timestamp
+        , json_extract(data, ?) AS value 
+      FROM metrics 
+      WHERE TIMESTAMP > datetime('now', ?)
+      ORDER BY timestamp ASC'''
+    
+    print("Executing query:", query)
+    print("With parameters:", (f'$.{key}', f'-{dateRange} minutes'))
+
+    c.execute(query, (f'$."{key}"',f'-{dateRange} minutes'))
+    rows = c.fetchall()
+    conn.close()
+    
+    # Prepare data for Chart.js
+    labels = [row[0] for row in rows]
+    data = [row[1] for row in rows]
+    
+    chart_data = {
+        "labels": labels,
+        "datasets": [
+            {
+                "label": key,
+                "data": data
+            }
+        ]
+    }
+    
+    return jsonify(chart_data)
+
+@app.route('/metrics/keys')
+def metrics_keys():
+    conn = sqlite3.connect(database=database_name)
+    c = conn.cursor()
+    
+    # Select all keys from the JSON data
+    c.execute('''
+        SELECT DISTINCT json_each.key
+        FROM metrics, json_each(metrics.data)
+        ORDER BY json_each.key
+    ''')
+    rows = c.fetchall()
+    conn.close()
+    
+    # Convert the list of tuples to a list of keys
+    keys = [row[0] for row in rows]
+    
+    return jsonify(keys)
 if __name__ == '__main__':
+
+    # Connect to the SQLite database
+
+    conn = sqlite3.connect(database_name)
+    c = conn.cursor()
+
+    # Create a table with a JSON column
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS metrics (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+            data TEXT  -- Store JSON data as TEXT
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
     print ("Starting UPS monitor")
-    while True:
-        status = get_ups_status(host=ups_host, login=ups_login, password=ups_password)
-        if status:
-            print(status)
-            send_metrics(status)
-        else: 
-            print('Battery not found')
-        time.sleep(300)
+    # Run this block in a background thread
+    background_thread = threading.Thread(name='Background Monitor', target=background_ups_monitor, daemon=True)
+    background_thread.start()
+    app.run(host='0.0.0.0',port=5001, debug=True)
+
+    
